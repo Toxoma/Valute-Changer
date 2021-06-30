a8295aa308e1e7ccf83dac139809e6fbab28f6c4
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-30 22:38:52 +0300

    lesson29

--- script.js
+++ script.js
@@ -3,45 +3,63 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const select = document.getElementById('cars'),
-        output = document.getElementById('output');
 
-    const filterData = (data) => {
-        data.cars.forEach(item => {
-            if (item.brand === select.value) {
-                const {
-                    brand,
-                    model,
-                    price
-                } = item;
-                output.innerHTML = `Тачка ${brand} ${model} <br>
-                    Цена: ${price}$`;
-            }
+    const select = document.getElementById('valuta');
+    const sections = document.querySelectorAll('.block'),
+        usdStr = 'Доллар США (USD)',
+        eurStr = 'Евро Европа (EUR)';
+
+    let current ="USD",
+        currentValue;
+
+
+    const getData = () => {
+        return fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+    };
+
+    const changeText = (value) => {
+        const labels = document.querySelectorAll('.change-text');
+        labels.forEach(label => {
+            label.textContent = value == "USD" ? usdStr : eurStr;
         });
     };
 
-    const promise = new Promise((resolve, reject) => {
-        select.addEventListener('change', () => {
-
-            const request = new XMLHttpRequest();
-
-            request.addEventListener('readystatechange', () => {
-                if (request.readyState !== 4) {
-                    return;
-                }
-                if (request.status === 200) {
-                    const data = JSON.parse(request.responseText);
-                    resolve(data);
-                } else {
-                    reject('Произошла ошибка');
-                }
-            });
-
-            request.open('GET', './cars.json');
-            request.setRequestHeader('Content-type', 'application/json');
-            request.send();
-        });
+    select.addEventListener('change', (e) => {
+        current = e.target.value;
+        changeText(current);
+        update(current);
     });
 
-    promise.then(filterData).catch(err => console.error(err));
+    sections.forEach(section => {
+        const btn = section.querySelector('.btn');
+
+        btn.addEventListener('click', () => {
+            const input = section.querySelector('.write'),
+            output = section.querySelector('.output');
+            if (section.classList.contains('block-1')) {
+                output.value = parseFloat(+input.value*currentValue).toFixed(2);
+            }else{
+                output.value = parseFloat(+input.value/currentValue).toFixed(2);
+            }
+        });
+
+    });
+
+    const update = (money)=>{
+        getData()
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const arr = Object.values(data.Valute);
+            currentValue = arr.filter(item => item.CharCode === money)[0].Value;
+            return currentValue;
+        })
+        .catch(err => {
+            console.error('!!!' + err);
+        });
+    };
+
+    update(current);
+
 });
